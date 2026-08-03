c5bfeb968ddb588fccf30f0ff10dd73b94ea6cf0
almosowy85 <oilman852@gmail.com>
2026-08-03 21:08:37 +0300

    Update books-data.js

--- books-data.js
+++ books-data.js
@@ -1,1 +1,15 @@
 
+const books = [
+
+{
+title: "نهج البلاغة",
+author: "الإمام علي عليه السلام",
+pages: "520",
+year: "2024",
+category: "حديث",
+description: "مجموعة من الخطب والرسائل والحكم.",
+image: "images/nahj.jpg",
+pdf: "pdf/nahj.pdf"
+}
+
+];
