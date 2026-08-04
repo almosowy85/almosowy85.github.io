60dde22a6cbafa07d8fe074410f80eb3b7f35e0f
almosowy85 <oilman852@gmail.com>
2026-08-04 14:50:37 +0300

    Update articles-data.js

--- articles-data.js
+++ articles-data.js
@@ -1,13 +1,20 @@
 const articles = [
-
-{
-title: "الإمام الحسن المجتبى عليه السلام",
-author: "محمد الموسوي",
-date: "2026-08-04",
-category: "سيرة",
-description: "نبذة مختصرة عن حياة الإمام الحسن عليه السلام.",
-image: "images/imam-hassan.jpeg",
-file: "articles/imam-hassan.md"
-}
-
+    {
+        title: "الإمام الحسن المجتبى عليه السلام",
+        author: "محمد الموسوي",
+        date: "2026-08-04",
+        category: "سيرة",
+        description: "نبذة مختصرة عن حياة الإمام الحسن عليه السلام.",
+        image: "images/imam-hassan.jpeg",
+        file: "articles/imam-hassan.md"
+    },
+    {
+        title: "عنوان المقال الجديد",
+        author: "محمد الموسوي",
+        date: "2026-08-05",
+        category: "عقائد",
+        description: "وصف مختصر للمقال الجديد...",
+        image: "images/new-image.jpeg",
+        file: "articles/new-article.md"
+    }
 ];
