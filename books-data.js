6c6ff7603a75d684443d3e9b52a1a9b5d1fd7d68
almosowy85 <oilman852@gmail.com>
2026-08-05 14:54:58 +0300

    Update books-data.js

--- books-data.js
+++ books-data.js
@@ -1,35 +1,22 @@
 const books = [
-
-{
-title: "علل الشرائع",
-author: "الشيخ الصدوق",
-pages: "—",
-year: "—",
-category: "حديث",
-description: "كتاب علل الشرائع للشيخ الصدوق.",
-image:"images/elel-alsharaie.jpeg", 
-pdf: "pdf/elel-alsharaie (1).pdf"
-}
- , 
-{
-title: "نهج البلاغة",
-author: "الامام علي",
-pages: "٨٤١",
-year: "",
-category: "",
-description: "",
-image: "images/nahj.jpg",
-pdf: "pdf/nahj1.pdf"
-}
- , 
-{
-title: "",
-author: "",
-pages: "",
-year: "",
-category: "",
-description: "",
-image: "images/.jpeg",
-pdf: "pdf/.pdf"
-} 
+  {
+    title: "علل الشرائع",
+    author: "الشيخ الصدوق",
+    pages: "—",
+    year: "—",
+    category: "حديث",
+    description: "كتاب علل الشرائع للشيخ الصدوق.",
+    image: "images/elel-alsharaie.jpeg",
+    pdf: "pdf/elel-alsharaie (1).pdf"
+  },
+  {
+    title: "نهج البلاغة",
+    author: "الإمام علي عليه السلام",
+    pages: "841",
+    year: "—",
+    category: "عقائد وخطب",
+    description: "كتاب نهج البلاغة - شريف الرضي.",
+    image: "images/nahj.jpg", 
+    pdf: "pdf/nahj1.pdf"
+  }
 ];
